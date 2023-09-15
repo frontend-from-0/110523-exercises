7826d4ed8d4bd531dc0e8005e268577d29ca8c74
glcn375 <gulcin_375@hotmail.com>
2023-09-15 20:26:28 +0300

    js codes are added

--- JS/Lesson14/gulcin/script.js
+++ JS/Lesson14/gulcin/script.js
@@ -39,17 +39,23 @@ console.log('Exercise 3.2:', number3);
 
 console.log('------------------------');
 // 4. Declare a variable firstName and initialize it to your first name as a string.
+const firstName = 'Gülçin';
 // Declare a variable lastName and initialize it to your last name as a string.
+const lastName = ' Gülcü';
 // Declare a variable fullName and initialize it to the concatenation of firstName and lastName.
+const fullName = firstName + lastName;
 
-console.log('Exercise 4:' /* Result here */);
+console.log('Exercise 4:', fullName);
 
 console.log('------------------------');
 // 5. Declare a variable number4 and initialize it to 7.
+const number4 = 7;
 // Declare a variable number5 and initialize it to 2.
+const number5 = 2;
 // Declare a variable result2 and initialize it to the product of number4 and number5 (multiply numbers).
+const result2 = number4 * number5;
 
-console.log('Exercise 5:'  /* Result here */);
+console.log('Exercise 5:',  result2);
  
 console.log('------------------------');
 // 6. Declare a variable PI and initialize it to the value of PI (3.14159). There's a built in function to do that (Math.PI).
@@ -76,9 +82,12 @@ console.log('Exercise 7.7:', counter);
 
 console.log('------------------------');
 // 8. Declare a variable temperature and initialize it to 20.
-// Increase the value of temperature by 5 and log the result to console.
+let temperature = 20;
 
-console.log('Exercise 8:'  /* Result here */);
+// Increase the value of temperature by 5 and log the result to console.
+temperature += 5;
+
+console.log('Exercise 8:', temperature);
 
 console.log('------------------------');
 // 9. Declare a variable TODAY_DATE and initialize it to the current date using the built in Date object.
@@ -116,13 +125,21 @@ console.log('------------------------');
 
 // 12. Declare a variable number10 and initialize it to -3. Use the postfix ++ operator to increment the value of num by 1, then multiply the result by 2 and log it to the console.
 
-console.log('Exercise 12:'  /* Result here */);
+let number12 = -3;
+number12++;
+const result3 = number12 * 2;
+
+console.log('Exercise 12:', result3);
 
 console.log('------------------------');
 // 13. Declare two variables a and b and initialize them to 2 and 3, respectively. Use the prefix ++ operator to increment the value of a by 1, then add b to the result and log it to the console.
+let a = 2;
+const b = 3;
+++a;
+const result4 = a + b;
 
 
-console.log('Exercise 13:'  /* Result here */);
+console.log('Exercise 13:', result4);
 
 console.log('------------------------');
 
