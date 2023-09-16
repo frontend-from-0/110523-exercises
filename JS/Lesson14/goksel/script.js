1910653f7e3dbc51906a1c1be5d5aec8c7c8a7ad
goksel agan <gokselagan@outlook.com>
2023-09-16 23:27:30 +0300

    goksel JS/Lesson14 exercises done

--- JS/Lesson14/goksel/script.js
+++ JS/Lesson14/goksel/script.js
@@ -11,9 +11,9 @@ console.log('Exercise 1:', z);
 
 console.log('------------------------');
 // 2. Declare a variable name and initialize it to your name as a string.
-const myName = 'Gülçin';
+const myName = 'Goksel';
 // Declare a variable age and initialize it to your age as a number.
-const age = 30;
+const age = 32;
 // Declare a variable phrase and initialize it to the string "Hello, world!".
 const phrase = 'Hello, world!';
 // Add name and age to the sentence above. "Hello, world! My name is (name). I'm (age) years old.".
@@ -27,7 +27,7 @@ const number1 = 10;
 // Declare a variable number2 and initialize it to 3.
 const number2 = 3;
 // Declare a variable result and initialize it to the quotient of number1 divided by number2.
-const result = number1/number2;
+const result = number1 / number2;
 // Declare a variable number3 and initialize it to the difference between number1 and number2.
 const number3= number1-number2 ;
 
@@ -41,15 +41,21 @@ console.log('------------------------');
 // 4. Declare a variable firstName and initialize it to your first name as a string.
 // Declare a variable lastName and initialize it to your last name as a string.
 // Declare a variable fullName and initialize it to the concatenation of firstName and lastName.
+const myFirstName = "Goksel" ;
+const myLastName = "AGAN" ;
+const myFullName = myFirstName + myLastName ;
 
-console.log('Exercise 4:' /* Result here */);
+console.log('Exercise 4:', myFullName);
 
 console.log('------------------------');
 // 5. Declare a variable number4 and initialize it to 7.
 // Declare a variable number5 and initialize it to 2.
 // Declare a variable result2 and initialize it to the product of number4 and number5 (multiply numbers).
+const number4 = 7 ;
+const number5 = 2 ;
+const result2 = number4 * number5 ;
 
-console.log('Exercise 5:'  /* Result here */);
+console.log('Exercise 5:', result2);
  
 console.log('------------------------');
 // 6. Declare a variable PI and initialize it to the value of PI (3.14159). There's a built in function to do that (Math.PI).
@@ -78,7 +84,11 @@ console.log('------------------------');
 // 8. Declare a variable temperature and initialize it to 20.
 // Increase the value of temperature by 5 and log the result to console.
 
-console.log('Exercise 8:'  /* Result here */);
+let temperature = 20 ;
+temperature += 5;
+
+
+console.log('Exercise 8:', temperature);
 
 console.log('------------------------');
 // 9. Declare a variable TODAY_DATE and initialize it to the current date using the built in Date object.
@@ -115,14 +125,19 @@ console.log('Exercise11:', number10, number11);
 console.log('------------------------');
 
 // 12. Declare a variable number10 and initialize it to -3. Use the postfix ++ operator to increment the value of num by 1, then multiply the result by 2 and log it to the console.
-
-console.log('Exercise 12:'  /* Result here */);
+let number30 = -3 ;
+number30 ++ ;
+let result20 = number30 * 2 ;
+console.log('Exercise 12:', result20);
 
 console.log('------------------------');
 // 13. Declare two variables a and b and initialize them to 2 and 3, respectively. Use the prefix ++ operator to increment the value of a by 1, then add b to the result and log it to the console.
+let a = 2 ;
+let b = 3 ;
+a ++ ;
+let result44 = a + b ;
 
-
-console.log('Exercise 13:'  /* Result here */);
+console.log('Exercise 13:' , result44);
 
 console.log('------------------------');
 
