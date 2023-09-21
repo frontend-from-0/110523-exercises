74b89ac3e44f1f19c54312607db2c2b70f2a2c00
Esruzn07 <esrauzun07@gmail.com>
2023-09-21 23:43:49 +0300

    esra lesson15

--- JS/Lesson15/esra/script.js
+++ JS/Lesson15/esra/script.js
@@ -15,12 +15,12 @@ console.log('------------------------');
 // 3. Use the `toUpperCase` method to convert a string to uppercase and print it to the console.
 const stringExercise3 =
 	'Python is a popular language for data analysis and machine learning.';
-console.log('exercise3: ' /* Result here */);
+console.log('exercise3: ', stringExercise3.toUpperCase());
 console.log('------------------------');
 // 4. Use the `toLowerCase` method to convert a string to lowercase and print it to the console.
 const stringExercise4 =
 	'FUNCTIONAL PROGRAMMING EMPHASIZES PURE FUNCTIONS AND IMMUTABLE DATA.';
-console.log('Exercise4: ' /* Result here */);
+console.log('Exercise4: ', stringExercise4.toLowerCase());
 
 console.log('------------------------');
 // 5.a Use the `indexOf` method to find the index of "l" character in the string stringExercise5
@@ -37,7 +37,7 @@ console.log('Exercise5.b: ', stringExercise5.indexOf("language"));
 console.log('------------------------');
 // 6. Use the `lastIndexOf` method to find the last index of of "l" character in the string stringExercise5 and print it to the console.
 
-console.log('Exercise6: ' /* Result here */);
+console.log('Exercise6: ', stringExercise5.lastIndexOf(l));
 console.log('------------------------');
 // 7. Use the `charAt` method to retrieve first, 10th, 3rd from the end and the last
 // characters from the string stringExercise7 and print them to the console.
@@ -79,8 +79,8 @@ const string1Exercise12 = '$9.99 - Sale! 50% off all items in store';
 const string2Exercise12 =
 	'Bundle deal: Save $50 when you buy two products together!';
 
-console.log('Exercise12: ' /* Result here */);
-console.log('Exercise12: ' /* Result here */);
+console.log('Exercise12: ', string1Exercise12.endsWith(!));
+console.log('Exercise12: ', string2Exercise12.endsWith(!));
 
 console.log('------------------------');
 // 13. Use the `includes` method to check if a string includes a "%" character and print
@@ -113,31 +113,31 @@ console.log('------------------------');
 // 16. Extract the price and currency from a string below using "slice" method and print it to console.
 const planPriceString = 'Premium plan - 9.99 USD/month.'; // Should return "9.99 USD"
 
-console.log('Exercise16: ' /* Result here */);
+console.log('Exercise16: ', planPriceString.slice(14, 23));
 console.log('------------------------');
 
 // 17. Use the `repeat` method to create a string of asterisks (*) that forms a horizontal line with a length of 10 and print it to the console.
-
-console.log('Exercise17: ', /* Result here */);
+const stringExercise17 = "*"
+console.log('Exercise17: ', stringExercise17.repeat(10));
 
 console.log('------------------------');
 // 18. Use the `concat` method to combine three separate strings into one and print the result to the console.
 const string1Exercise18 = 'Hello, ';
 const string2Exercise18 = 'world';
 const string3Exercise18 = '!';
-console.log('Exercise18: ', /* Result here */);
+console.log('Exercise18: ', string1Exercise18.concat(' ', string2Exercise18, string3Exercise18));
 
 console.log('------------------------');
 // 19. Use the `split` and `join` methods to reverse the words in the string stringExercise28 and print the result to the console.
 const stringExercise19 = 'This is a sample sentence.';
-console.log('Exercise19: ', /* Result here */);
+console.log('Exercise19: ', stringExercise19.split(' '), stringExercise19.join(' '));
 
 console.log('------------------------');
 
 // 20. Use the `charAt` method to extract the first and last characters of a string and concatenate them into a new string. Then use the `toUpperCase` method to make the new string uppercase and print the result to the console.
 const stringExercise20 = 'JavaScript';
 
-console.log('Exercise20: ', /* Result here */);
+console.log('Exercise20: ', stringExercise20.charAt(0, 10), stringExercise20.toUpperCase());
 
 console.log('------------------------');
 // 21. Use the `substring` method to extract the substring "quick brown fox" from the string stringExercise32 based on two indices and print it to the console.
@@ -150,15 +150,15 @@ console.log('------------------------');
 const url1Exercise22 = 'https://www.example.com';
 const url2Exercise22 = 'http://www.example.org';
 
-console.log('Exercise22: URL 1 is valid:', /* Result here */);
-console.log('Exercise22: URL 2 is valid:', /* Result here */);
+console.log('Exercise22: URL 1 is valid:', url1Exercise22.endsWith(".com"));
+console.log('Exercise22: URL 2 is valid:', url2Exercise22.endsWith(".org"));
 
 console.log('------------------------');
 // Bonus: 23. Use the `includes` method to check if a string includes both "apple" and "banana" (case-insensitive), and print the result to the console. Use && (logical end operator) to check if both result are true: e.g. const result = condition1 && condition2;
 const string1Exercise23 = 'I like apple and Banana';
 const string2Exercise23 = 'I like apple and banana';
 const string3Exercise23 = 'I like Apple and banana';
-
-console.log('Exercise23: ', /* Result here */);
+const result = condition1 && condition2;
+console.log('Exercise23: ', string1Exercise23.includes("apple, Banana"), string2Exercise23.includes("banana", "apple"), string3Exercise23.includes("Apple", "banana"));
 
 console.log('------------------------');
