611a20bc4cf66a04f8935b8a3f7556664838614e
Gokselagan <132660570+Gokselagan@users.noreply.github.com>
2023-11-30 23:30:01 +0300

    Changes have been made

--- JS/Lesson22/goksel/script.js
+++ JS/Lesson22/goksel/script.js
@@ -6,19 +6,6 @@ buttons.forEach((btn,index)  => {
     btn.addEventListener("click",function(){
         contents[index].classList.toggle("active");
         btn.classList.toggle("active");
-
-
-        if(contents[index].classList.contains("active")){
-            btn.style.color = "#ffff00";
-            btn.style.fontWeight = "800";
-            btn.style.fontSize = "18px";
-            
-        }else {
-            btn.style.color = "";
-            btn.style.fontSize = "";
-            btn.style.fontWeight = "";
-            
-        }
     })
 });
 
