e16d882b72d51feb4aabc85e36f6e2dbf1c3f0fb
Gokselagan <132660570+Gokselagan@users.noreply.github.com>
2024-01-09 20:01:19 +0300

    goksel Lesson30 project done

---
+++ JS/Lesson30/goksel/assignment-files/job-board-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Card } from './components/Card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <img src='./images/bg-header-desktop.svg' ></img>
+      </header>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ JS/Lesson30/goksel/assignment-files/job-board-app/src/components/Card/index.jsx
@@ -0,0 +1,208 @@
+import "./styles.css";
+
+export const Card = () => {
+
+    const data = [
+        {
+            "id": 1,
+            "company": "Photosnap",
+            "logo": "./images/photosnap.svg",
+            "new": true,
+            "featured": true,
+            "position": "Senior Frontend Developer",
+            "role": "Frontend",
+            "level": "Senior",
+            "postedAt": "1d ago",
+            "contract": "Full Time",
+            "location": "USA Only",
+            "languages": ["HTML", "CSS", "JavaScript"],
+            "tools": []
+        },
+        {
+            "id": 2,
+            "company": "Manage",
+            "logo": "./images/manage.svg",
+            "new": true,
+            "featured": true,
+            "position": "Fullstack Developer",
+            "role": "Fullstack",
+            "level": "Midweight",
+            "postedAt": "1d ago",
+            "contract": "Part Time",
+            "location": "Remote",
+            "languages": ["Python"],
+            "tools": ["React"]
+        },
+        {
+            "id": 3,
+            "company": "Account",
+            "logo": "./images/account.svg",
+            "new": true,
+            "featured": false,
+            "position": "Junior Frontend Developer",
+            "role": "Frontend",
+            "level": "Junior",
+            "postedAt": "2d ago",
+            "contract": "Part Time",
+            "location": "USA Only",
+            "languages": ["JavaScript"],
+            "tools": ["React", "Sass"]
+        },
+        {
+            "id": 4,
+            "company": "MyHome",
+            "logo": "./images/myhome.svg",
+            "new": false,
+            "featured": false,
+            "position": "Junior Frontend Developer",
+            "role": "Frontend",
+            "level": "Junior",
+            "postedAt": "5d ago",
+            "contract": "Contract",
+            "location": "USA Only",
+            "languages": ["CSS", "JavaScript"],
+            "tools": []
+        },
+        {
+            "id": 5,
+            "company": "Loop Studios",
+            "logo": "./images/loop-studios.svg",
+            "new": false,
+            "featured": false,
+            "position": "Software Engineer",
+            "role": "Fullstack",
+            "level": "Midweight",
+            "postedAt": "1w ago",
+            "contract": "Full Time",
+            "location": "Worldwide",
+            "languages": ["JavaScript", "Ruby"],
+            "tools": ["Sass"]
+        },
+        {
+            "id": 6,
+            "company": "FaceIt",
+            "logo": "./images/faceit.svg",
+            "new": false,
+            "featured": false,
+            "position": "Junior Backend Developer",
+            "role": "Backend",
+            "level": "Junior",
+            "postedAt": "2w ago",
+            "contract": "Full Time",
+            "location": "UK Only",
+            "languages": ["Ruby"],
+            "tools": ["RoR"]
+        },
+        {
+            "id": 7,
+            "company": "Shortly",
+            "logo": "./images/shortly.svg",
+            "new": false,
+            "featured": false,
+            "position": "Junior Developer",
+            "role": "Frontend",
+            "level": "Junior",
+            "postedAt": "2w ago",
+            "contract": "Full Time",
+            "location": "Worldwide",
+            "languages": ["HTML", "JavaScript"],
+            "tools": ["Sass"]
+        },
+        {
+            "id": 8,
+            "company": "Insure",
+            "logo": "./images/insure.svg",
+            "new": false,
+            "featured": false,
+            "position": "Junior Frontend Developer",
+            "role": "Frontend",
+            "level": "Junior",
+            "postedAt": "2w ago",
+            "contract": "Full Time",
+            "location": "USA Only",
+            "languages": ["JavaScript"],
+            "tools": ["Vue", "Sass"]
+        },
+        {
+            "id": 9,
+            "company": "Eyecam Co.",
+            "logo": "./images/eyecam-co.svg",
+            "new": false,
+            "featured": false,
+            "position": "Full Stack Engineer",
+            "role": "Fullstack",
+            "level": "Midweight",
+            "postedAt": "3w ago",
+            "contract": "Full Time",
+            "location": "Worldwide",
+            "languages": ["JavaScript", "Python"],
+            "tools": ["Django"]
+        },
+        {
+            "id": 10,
+            "company": "The Air Filter Company",
+            "logo": "./images/the-air-filter-company.svg",
+            "new": false,
+            "featured": false,
+            "position": "Front-end Dev",
+            "role": "Frontend",
+            "level": "Junior",
+            "postedAt": "1mo ago",
+            "contract": "Part Time",
+            "location": "Worldwide",
+            "languages": ["JavaScript"],
+            "tools": ["React", "Sass"]
+        }
+    ];
+
+    return (
+        <div className="main">
+
+            {
+                data.map((item) => (
+
+                    <div className="card">
+
+                        <div className="left-side">
+                            <div className="company-logo">
+                                <img src={item.logo} alt="company-img" />
+                            </div>
+
+                            <div className="informations">
+                                <div className="head">
+                                    <span className="company-name" > {item.company} </span>
+                                    {item.new && <button className="new" >NEW!</button>}
+                                    {item.featured && <button className="featured">FEATURED</button>}
+                                </div>
+                                <div className="position">
+                                    <span> {item.position} </span>
+                                </div>
+                                <div className="footer-informations">
+                                    <span> {item.postedAt} </span>
+                                    <span className="point">.</span>
+                                    <span> {item.contract} </span>
+                                    <span className="point">.</span>
+                                    <span> {item.location} </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="right-side">
+                            <div className="abilities">
+                                <span> {item.role} </span>
+                                <span> {item.level} </span>
+                                {item.tools && item.tools.length > 0 && (
+                                    <span>{item.tools.join(' ')}</span>
+                                )}
+
+                                <span> {item.languages.join(" ")} </span>
+                            </div>
+                        </div>
+
+                    </div>
+                ))
+            }
+
+        </div>
+    )
+}
