0e9ca611dba8d15d51bd42a87a9fb5ec6da725e6
Gokselagan <gokselagan@outlook.com>
2024-02-12 18:50:54 +0300

    Lesson30-fixed

--- JS/Lesson30/goksel/assignment-files/job-board-app/src/components/Header/header.jsx
+++ JS/Lesson30/goksel/assignment-files/job-board-app/src/components/Header/header.jsx
@@ -1,11 +1,17 @@
 import headerImage from "../../images/bg-header-desktop.svg";
+import headerImageSmall from "../../images/bg-header-mobile.svg";
 import "./header.css";
 
 export const Header = () => {
 
     return (
         <header>
-            <img src={headerImage} alt='header-image' />
+            <picture >
+                <source media="(max-width: 639px)" srcSet={headerImageSmall} />
+                <img
+                    src={headerImage} alt='Abstract background in different shades of green' />
+            </picture>
+
         </header>
     )
 }
